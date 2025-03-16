a296c62312936450b17883d33729675f054a7699
abderrahim <elmoutaouakil12345@gmail.com>
2025-03-16 21:57:32 +0000

    add layout customization with Modifier

--- app/src/main/java/fr/iutlan/tp4/MainActivity.kt
+++ app/src/main/java/fr/iutlan/tp4/MainActivity.kt
@@ -7,31 +7,24 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import fr.iutlan.tp4.ui.theme.TP4Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        enableEdgeToEdge()
-//        setContent {
-//            TP4Theme {
-//                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-//                    Greeting(
-//                        name = "Android",
-//                        modifier = Modifier.padding(innerPadding)
-//                    )
-//                }
-//            }
-//        }
         setContent {
             Accueil(name = "numéro 6")
         }
@@ -39,14 +32,25 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Accueil(name: String) {
-    Row {
-        Text(text = "Bonjour $name", fontSize=20.sp)
-        Text(text = "Je vois de grands progrès", color = Color.Blue)
+fun Accueil(name: String, modifier: Modifier = Modifier) {
+    ElevatedCard {
+        Column(
+            modifier = modifier.padding(8.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = "Bonjour $name",
+                fontSize = 20.sp,
+                modifier = Modifier.padding(12.dp))
+            Text(text = "Je vois de grands progrès", color = Color.Blue)
+        }
     }
 }
 @Preview
 @Composable
 fun AccueilPreview() {
-    Accueil(name = "numéro 10")
+    Column {
+        Accueil(name = "numéro 10", modifier = Modifier.fillMaxWidth())
+        Accueil(name = "numéro 6") // valeur par défaut du modifier
+    }
 }
